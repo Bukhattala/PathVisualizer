39464fb1707e7ffa8b6f1ae01f7d144fa7899302
Ubuntu <ubuntu@ip-172-31-19-49.ec2.internal>
2022-03-18 19:58:36 +0000

    Created a node component. Added some CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Node from './Node/Node';
 
 function App() {
+  const nodes = useState([])
+
+  const buildGrid = () => {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sorry about that
-        </a>
-      </header>
+      <Node/>
     </div>
   );
 }
